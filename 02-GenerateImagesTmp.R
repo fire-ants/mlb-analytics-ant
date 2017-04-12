f2694fab5108aae00e6357fe13a1aaa0c96b5a0e
jrbattles <jason.r.battles@gmail.com>
2017-04-12 17:12:17 -0500

    HitterVal visualizations working now

--- 02-GenerateImagesTmp.R
+++ 02-GenerateImagesTmp.R
@@ -8,10 +8,10 @@ library(RColorBrewer)
 library(akima)
 #library(wesanderson)
 
-create_plots <- function(data, mlbID, ...) {
+create_heatmap_plots <- function(data, mlbID, ...) {
     # data <- joined
     
-    filename = str_c(mlbID,"-ALL.png")
+    #filename = str_c(mlbID,"-ALL.png")
     png(filename)
     brewer.pal(3, "RdBu")
     #wes_palette("Zissou")
@@ -32,12 +32,26 @@ create_plots <- function(data, mlbID, ...) {
     sub.FF <- joined %>% filter(pitch_type=="FF")
     sub.FF.RHP <- sub.FF %>% filter(p_throws=="R")
     sub.FF.LHP <- sub.FF %>% filter(p_throws=="L")
+    
     sub.SL <- joined %>% filter(pitch_type=="SL")
+    sub.SL.RHP <- sub.SL %>% filter(p_throws=="R")
+    sub.SL.LHP <- sub.SL %>% filter(p_throws=="L")
+    
     sub.CH <- joined %>% filter(pitch_type=="CH")
+    sub.CH.RHP <- sub.CH %>% filter(p_throws=="R")
+    sub.CH.LHP <- sub.CH %>% filter(p_throws=="L")
+    
+    
     sub.CU <- joined %>% filter(pitch_type=="CU")
+    sub.CU.RHP <- sub.CU %>% filter(p_throws=="R")
+    sub.CU.LHP <- sub.CU %>% filter(p_throws=="L")
+    
     #sub.FC <- joined %>% filter(batter=="545361",pitch_type=="FC")
     #sub.FT <- joined %>% filter(batter=="545361",pitch_type=="FT")
     sub.SI <- joined %>% filter(pitch_type=="SI")
+    sub.SI.RHP <- sub.SI %>% filter(p_throws=="R")
+    sub.SI.LHP <- sub.SI %>% filter(p_throws=="L")
+    
     #sub.FS <- joined %>% filter(batter=="545361",pitch_type=="FS")
     
     ## FF Four-seam fastball - RHP
@@ -80,6 +94,52 @@ create_plots <- function(data, mlbID, ...) {
     
     ## Save plot to working directory in the plots sub-folder
     filename = str_c(mlbID,"-lhp-hm-FF.png")
+    #png(filename)
+    ggsave(filename, device="png", path="plots/")
+    dev.off()
+    
+    
+    
+    ## SL Slider - RHP
+    hv.SL <- data.frame(x = sub.SL.RHP$px, y = sub.SL.RHP$pz, z = sub.SL.RHP$hitter_val)
+    hv.SL.grid <- interp(hv.SL$x, hv.SL$y, hv.SL$z)
+    hv.SL.grid2 <- expand.grid(x=hv.SL.grid$x, y=hv.SL.grid$y)
+    hv.SL.grid2$z <- as.vector(hv.SL.grid$z)
+    
+    b1 <- colorRampPalette(c("navy","royalblue","white"))(200)
+    re <- colorRampPalette(c("white","red2","darkred"))(200)
+    
+    ggplot(hv.SL.grid2) + labs(x="x pos",y="z pos") + ggtitle("SL Hitter Value - RHP") + 
+        geom_tile(aes(x = x, y = y, z = z, fill = z) ) + coord_equal() + 
+        geom_contour(aes(x = x, y = y, z = z, fill = z), color = "white", alpha = .3) + 
+        scale_fill_gradientn(name="Hitter\nValue",colors=c(b1,"white",re), na.value="white", limits=c(min(data$hitter_val),max(data$hitter_val))) + 
+        geom_path(aes(x, y), data = kZone, linetype = 2) + coord_cartesian(xlim=c(-1.5,1.5),ylim=c(1,4)) + 
+        theme_bw()
+    
+    ## Save plot to working directory in the plots sub-folder
+    filename = str_c(mlbID,"-rhp-hm-SL.png")
+    #png(filename)
+    ggsave(filename, device="png", path="plots/")
+    dev.off()
+    
+    ## SL Slider - LHP
+    hv.SL <- data.frame(x = sub.SL.LHP$px, y = sub.SL.LHP$pz, z = sub.SL.LHP$hitter_val)
+    hv.SL.grid <- interp(hv.SL$x, hv.SL$y, hv.SL$z)
+    hv.SL.grid2 <- expand.grid(x=hv.SL.grid$x, y=hv.SL.grid$y)
+    hv.SL.grid2$z <- as.vector(hv.SL.grid$z)
+    
+    b1 <- colorRampPalette(c("navy","royalblue","white"))(200)
+    re <- colorRampPalette(c("white","red2","darkred"))(200)
+    
+    ggplot(hv.SL.grid2) + labs(x="x pos",y="z pos") + ggtitle("SL Hitter Value - LHP") + 
+        geom_tile(aes(x = x, y = y, z = z, fill = z) ) + coord_equal() + 
+        geom_contour(aes(x = x, y = y, z = z, fill = z), color = "white", alpha = .3) + 
+        scale_fill_gradientn(name="Hitter\nValue",colors=c(b1,"white",re), na.value="white", limits=c(min(data$hitter_val),max(data$hitter_val))) + 
+        geom_path(aes(x, y), data = kZone, linetype = 2) + coord_cartesian(xlim=c(-1.5,1.5),ylim=c(1,4)) + 
+        theme_bw()
+    
+    ## Save plot to working directory in the plots sub-folder
+    filename = str_c(mlbID,"-lhp-hm-SL.png")
     #png(filename)
     ggsave(filename, device="png", path="plots/")
     dev.off()
@@ -145,5 +205,5 @@ for (mlbID in hitters) {
              density2 = list(quant_score = 1), layer = facet_grid(. ~ p_throws, labeller = labeller(p_throws = pitch_label)))
     
     # generate plots
-    create_plots(joined,mlbID)
+    create_heatmap_plots(joined,mlbID)
 }

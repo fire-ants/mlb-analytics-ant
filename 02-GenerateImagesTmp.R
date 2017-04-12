22fa704a83718aada99a0eb8e32394339626eb3e
Cohen <dan.cohen@emc.com>
2017-04-12 16:50:02 -0600

    hitterval viz updates

--- 02-GenerateImagesTmp.R
+++ 02-GenerateImagesTmp.R
@@ -8,16 +8,12 @@ library(RColorBrewer)
 library(akima)
 #library(wesanderson)
 
-create_heatmap_plots <- function(data, mlbID, ...) {
-    # data <- joined
+create_hv_plots <- function(data, mlbID, ...) {
     
-    #filename = str_c(mlbID,"-ALL.png")
-    png(filename)
-    brewer.pal(3, "RdBu")
-    #wes_palette("Zissou")
-    #pal <- wes_palette("Zissou", 100, type = "continuous")
+#    png(filename)
+#    brewer.pal(3, "RdBu")
+    
     ## Add Batter's strike zone
-    ## not required if loading workspace workspace_tables_with_pitch_type
     topKzone = mean(data$sz_top, na.rm = TRUE)
     botKzone = mean(data$sz_bot, na.rm = TRUE) 
     inKzone = -.95
@@ -28,7 +24,6 @@ create_heatmap_plots <- function(data, mlbID, ...) {
     )
     
     ## Filter out by Batter and Pitch Type
-    ## not required if loading workspace workspace_tables_with_pitch_type
     sub.FF <- joined %>% filter(pitch_type=="FF")
     sub.FF.RHP <- sub.FF %>% filter(p_throws=="R")
     sub.FF.LHP <- sub.FF %>% filter(p_throws=="L")
@@ -41,20 +36,15 @@ create_heatmap_plots <- function(data, mlbID, ...) {
     sub.CH.RHP <- sub.CH %>% filter(p_throws=="R")
     sub.CH.LHP <- sub.CH %>% filter(p_throws=="L")
     
-    
     sub.CU <- joined %>% filter(pitch_type=="CU")
     sub.CU.RHP <- sub.CU %>% filter(p_throws=="R")
     sub.CU.LHP <- sub.CU %>% filter(p_throws=="L")
     
-    #sub.FC <- joined %>% filter(batter=="545361",pitch_type=="FC")
-    #sub.FT <- joined %>% filter(batter=="545361",pitch_type=="FT")
     sub.SI <- joined %>% filter(pitch_type=="SI")
     sub.SI.RHP <- sub.SI %>% filter(p_throws=="R")
     sub.SI.LHP <- sub.SI %>% filter(p_throws=="L")
     
-    #sub.FS <- joined %>% filter(batter=="545361",pitch_type=="FS")
-    
-    ## FF Four-seam fastball - RHP
+## FF Four-seam fastball - RHP
     hv.FF <- data.frame(x = sub.FF.RHP$px, y = sub.FF.RHP$pz, z = sub.FF.RHP$hitter_val)
     hv.FF.grid <- interp(hv.FF$x, hv.FF$y, hv.FF$z)
     hv.FF.grid2 <- expand.grid(x=hv.FF.grid$x, y=hv.FF.grid$y)
@@ -71,10 +61,8 @@ create_heatmap_plots <- function(data, mlbID, ...) {
         theme_bw()
  
     ## Save plot to working directory in the plots sub-folder
-    filename = str_c(mlbID,"-rhp-hm-FF.png")
-    #png(filename)
+    filename = str_c(mlbID,"-rhp-hv-FF.png")
     ggsave(filename, device="png", path="plots/")
-    dev.off()
     
     ## FF Four-seam fastball - LHP
     hv.FF <- data.frame(x = sub.FF.LHP$px, y = sub.FF.LHP$pz, z = sub.FF.LHP$hitter_val)
@@ -93,14 +81,10 @@ create_heatmap_plots <- function(data, mlbID, ...) {
         theme_bw()
     
     ## Save plot to working directory in the plots sub-folder
-    filename = str_c(mlbID,"-lhp-hm-FF.png")
-    #png(filename)
+    filename = str_c(mlbID,"-lhp-hv-FF.png")
     ggsave(filename, device="png", path="plots/")
-    dev.off()
-    
-    
-    
-    ## SL Slider - RHP
+
+## SL Slider - RHP
     hv.SL <- data.frame(x = sub.SL.RHP$px, y = sub.SL.RHP$pz, z = sub.SL.RHP$hitter_val)
     hv.SL.grid <- interp(hv.SL$x, hv.SL$y, hv.SL$z)
     hv.SL.grid2 <- expand.grid(x=hv.SL.grid$x, y=hv.SL.grid$y)
@@ -117,10 +101,10 @@ create_heatmap_plots <- function(data, mlbID, ...) {
         theme_bw()
     
     ## Save plot to working directory in the plots sub-folder
-    filename = str_c(mlbID,"-rhp-hm-SL.png")
+    filename = str_c(mlbID,"-rhp-hv-SL.png")
     #png(filename)
     ggsave(filename, device="png", path="plots/")
-    dev.off()
+    #dev.off()
     
     ## SL Slider - LHP
     hv.SL <- data.frame(x = sub.SL.LHP$px, y = sub.SL.LHP$pz, z = sub.SL.LHP$hitter_val)
@@ -139,16 +123,138 @@ create_heatmap_plots <- function(data, mlbID, ...) {
         theme_bw()
     
     ## Save plot to working directory in the plots sub-folder
-    filename = str_c(mlbID,"-lhp-hm-SL.png")
+    filename = str_c(mlbID,"-lhp-hv-SL.png")
     #png(filename)
     ggsave(filename, device="png", path="plots/")
-    dev.off()
+    #dev.off()
+    
+## CH Four-seam fastball - RHP
+    hv.CH <- data.frame(x = sub.CH.RHP$px, y = sub.CH.RHP$pz, z = sub.CH.RHP$hitter_val)
+    hv.CH.grid <- interp(hv.CH$x, hv.CH$y, hv.CH$z)
+    hv.CH.grid2 <- expand.grid(x=hv.CH.grid$x, y=hv.CH.grid$y)
+    hv.CH.grid2$z <- as.vector(hv.CH.grid$z)
+    
+    b1 <- colorRampPalette(c("navy","royalblue","white"))(200)
+    re <- colorRampPalette(c("white","red2","darkred"))(200)
+    
+    ggplot(hv.CH.grid2) + labs(x="x pos",y="z pos") + ggtitle("CH Hitter Value - RHP") + 
+      geom_tile(aes(x = x, y = y, z = z, fill = z) ) + coord_equal() + 
+      geom_contour(aes(x = x, y = y, z = z, fill = z), color = "white", alpha = .3) + 
+      scale_fill_gradientn(name="Hitter\nValue",colors=c(b1,"white",re), na.value="white", limits=c(min(data$hitter_val),max(data$hitter_val))) + 
+      geom_path(aes(x, y), data = kZone, linetype = 2) + coord_cartesian(xlim=c(-1.5,1.5),ylim=c(1,4)) + 
+      theme_bw()
+    
+    ## Save plot to working directory in the plots sub-folder
+    filename = str_c(mlbID,"-rhp-hv-CH.png")
+    ggsave(filename, device="png", path="plots/")
+    
+    ## CH Four-seam fastball - LHP
+    hv.CH <- data.frame(x = sub.CH.LHP$px, y = sub.CH.LHP$pz, z = sub.CH.LHP$hitter_val)
+    hv.CH.grid <- interp(hv.CH$x, hv.CH$y, hv.CH$z)
+    hv.CH.grid2 <- expand.grid(x=hv.CH.grid$x, y=hv.CH.grid$y)
+    hv.CH.grid2$z <- as.vector(hv.CH.grid$z)
+    
+    b1 <- colorRampPalette(c("navy","royalblue","white"))(200)
+    re <- colorRampPalette(c("white","red2","darkred"))(200)
+    
+    ggplot(hv.CH.grid2) + labs(x="x pos",y="z pos") + ggtitle("CH Hitter Value - LHP") + 
+      geom_tile(aes(x = x, y = y, z = z, fill = z) ) + coord_equal() + 
+      geom_contour(aes(x = x, y = y, z = z, fill = z), color = "white", alpha = .3) + 
+      scale_fill_gradientn(name="Hitter\nValue",colors=c(b1,"white",re), na.value="white", limits=c(min(data$hitter_val),max(data$hitter_val))) + 
+      geom_path(aes(x, y), data = kZone, linetype = 2) + coord_cartesian(xlim=c(-1.5,1.5),ylim=c(1,4)) + 
+      theme_bw()
+    
+    ## Save plot to working directory in the plots sub-folder
+    filename = str_c(mlbID,"-lhp-hv-CH.png")
+    ggsave(filename, device="png", path="plots/")
+    
+## CU Four-seam fastball - RHP
+    hv.CU <- data.frame(x = sub.CU.RHP$px, y = sub.CU.RHP$pz, z = sub.CU.RHP$hitter_val)
+    hv.CU.grid <- interp(hv.CU$x, hv.CU$y, hv.CU$z)
+    hv.CU.grid2 <- expand.grid(x=hv.CU.grid$x, y=hv.CU.grid$y)
+    hv.CU.grid2$z <- as.vector(hv.CU.grid$z)
+    
+    b1 <- colorRampPalette(c("navy","royalblue","white"))(200)
+    re <- colorRampPalette(c("white","red2","darkred"))(200)
+    
+    ggplot(hv.CU.grid2) + labs(x="x pos",y="z pos") + ggtitle("CU Hitter Value - RHP") + 
+      geom_tile(aes(x = x, y = y, z = z, fill = z) ) + coord_equal() + 
+      geom_contour(aes(x = x, y = y, z = z, fill = z), color = "white", alpha = .3) + 
+      scale_fill_gradientn(name="Hitter\nValue",colors=c(b1,"white",re), na.value="white", limits=c(min(data$hitter_val),max(data$hitter_val))) + 
+      geom_path(aes(x, y), data = kZone, linetype = 2) + coord_cartesian(xlim=c(-1.5,1.5),ylim=c(1,4)) + 
+      theme_bw()
+    
+    ## Save plot to working directory in the plots sub-folder
+    filename = str_c(mlbID,"-rhp-hv-CU.png")
+    ggsave(filename, device="png", path="plots/")
+    
+    ## CU Four-seam fastball - LHP
+    hv.CU <- data.frame(x = sub.CU.LHP$px, y = sub.CU.LHP$pz, z = sub.CU.LHP$hitter_val)
+    hv.CU.grid <- interp(hv.CU$x, hv.CU$y, hv.CU$z)
+    hv.CU.grid2 <- expand.grid(x=hv.CU.grid$x, y=hv.CU.grid$y)
+    hv.CU.grid2$z <- as.vector(hv.CU.grid$z)
+    
+    b1 <- colorRampPalette(c("navy","royalblue","white"))(200)
+    re <- colorRampPalette(c("white","red2","darkred"))(200)
+    
+    ggplot(hv.CU.grid2) + labs(x="x pos",y="z pos") + ggtitle("CU Hitter Value - LHP") + 
+      geom_tile(aes(x = x, y = y, z = z, fill = z) ) + coord_equal() + 
+      geom_contour(aes(x = x, y = y, z = z, fill = z), color = "white", alpha = .3) + 
+      scale_fill_gradientn(name="Hitter\nValue",colors=c(b1,"white",re), na.value="white", limits=c(min(data$hitter_val),max(data$hitter_val))) + 
+      geom_path(aes(x, y), data = kZone, linetype = 2) + coord_cartesian(xlim=c(-1.5,1.5),ylim=c(1,4)) + 
+      theme_bw()
+    
+    ## Save plot to working directory in the plots sub-folder
+    filename = str_c(mlbID,"-lhp-hv-CU.png")
+    ggsave(filename, device="png", path="plots/")
+    
+    
+## SI Four-seam fastball - RHP
+    hv.SI <- data.frame(x = sub.SI.RHP$px, y = sub.SI.RHP$pz, z = sub.SI.RHP$hitter_val)
+    hv.SI.grid <- interp(hv.SI$x, hv.SI$y, hv.SI$z)
+    hv.SI.grid2 <- expand.grid(x=hv.SI.grid$x, y=hv.SI.grid$y)
+    hv.SI.grid2$z <- as.vector(hv.SI.grid$z)
+    
+    b1 <- colorRampPalette(c("navy","royalblue","white"))(200)
+    re <- colorRampPalette(c("white","red2","darkred"))(200)
+    
+    ggplot(hv.SI.grid2) + labs(x="x pos",y="z pos") + ggtitle("SI Hitter Value - RHP") + 
+      geom_tile(aes(x = x, y = y, z = z, fill = z) ) + coord_equal() + 
+      geom_contour(aes(x = x, y = y, z = z, fill = z), color = "white", alpha = .3) + 
+      scale_fill_gradientn(name="Hitter\nValue",colors=c(b1,"white",re), na.value="white", limits=c(min(data$hitter_val),max(data$hitter_val))) + 
+      geom_path(aes(x, y), data = kZone, linetype = 2) + coord_cartesian(xlim=c(-1.5,1.5),ylim=c(1,4)) + 
+      theme_bw()
+    
+    ## Save plot to working directory in the plots sub-folder
+    filename = str_c(mlbID,"-rhp-hv-SI.png")
+    ggsave(filename, device="png", path="plots/")
+    
+    ## SI Four-seam fastball - LHP
+    hv.SI <- data.frame(x = sub.SI.LHP$px, y = sub.SI.LHP$pz, z = sub.SI.LHP$hitter_val)
+    hv.SI.grid <- interp(hv.SI$x, hv.SI$y, hv.SI$z)
+    hv.SI.grid2 <- expand.grid(x=hv.SI.grid$x, y=hv.SI.grid$y)
+    hv.SI.grid2$z <- as.vector(hv.SI.grid$z)
+    
+    b1 <- colorRampPalette(c("navy","royalblue","white"))(200)
+    re <- colorRampPalette(c("white","red2","darkred"))(200)
+    
+    ggplot(hv.SI.grid2) + labs(x="x pos",y="z pos") + ggtitle("SI Hitter Value - LHP") + 
+      geom_tile(aes(x = x, y = y, z = z, fill = z) ) + coord_equal() + 
+      geom_contour(aes(x = x, y = y, z = z, fill = z), color = "white", alpha = .3) + 
+      scale_fill_gradientn(name="Hitter\nValue",colors=c(b1,"white",re), na.value="white", limits=c(min(data$hitter_val),max(data$hitter_val))) + 
+      geom_path(aes(x, y), data = kZone, linetype = 2) + coord_cartesian(xlim=c(-1.5,1.5),ylim=c(1,4)) + 
+      theme_bw()
+    
+    ## Save plot to working directory in the plots sub-folder
+    filename = str_c(mlbID,"-lhp-hv-SI.png")
+    ggsave(filename, device="png", path="plots/")
+    
     
     ## print(sprintf("aws s3api put-object --endpoint-url https://ecs2-us-central-1.emc.io/ --bucket fireants-dev --key %1$s-All.png --body %1$s-All.png", mlbID))
     #system(sprintf("aws s3api put-object --endpoint-url https://ecs2-us-central-1.emc.io/ --bucket fireants-dev --key %1$s-All.png --body %1$s-All.png", mlbID))
 }
 
-hittersLst <- c('547180','457705','502671','518626','502517','518934','467092','445988','471865','120074','514888')
+# hittersLst <- c('547180','457705','502671','518626','502517','518934','467092','445988','471865','120074','514888')
 hitters <- c('518626')
 #mlbdata <- pullMLBDataAndScore()
 mlbID <- '518626'
@@ -189,21 +295,21 @@ for (mlbID in hitters) {
     )
     
     # create Traditional Heat Map
-    strikeFX(subAllBallsInPlay, geom = "raster", density1 = list(type = "X"),
-             density2 = list(quant_score = 1), layer = facet_grid(pitch_type ~ p_throws, labeller = labeller(p_throws = pitch_label)))
-    
-    strikeFX(subAllBallsInPlay, geom = "raster", density1 = list(type = "X"),
-             density2 = list(quant_score = 1), layer = facet_grid(. ~ p_throws, labeller = labeller(p_throws = pitch_label)))
-    
-    strikeFX(subAllBallsInPlay, geom = "raster", density1 = list(type = "X"),
-             density2 = list(type = "X"), layer = facet_grid(. ~ stand, labeller = labeller(p_throws = pitch_label)))
-    
-    strikeFX(subAllBallsInPlay, geom = "raster", density1 = list(type = "X"),
-             density2 = list(quant_score = 1), layer = facet_grid(. ~ stand, labeller = labeller(p_throws = pitch_label)))
-    
-    strikeFX(subAllBallsInPlay, geom = "raster", density1 = list(type = "X"),
-             density2 = list(quant_score = 1), layer = facet_grid(. ~ p_throws, labeller = labeller(p_throws = pitch_label)))
+#   strikeFX(subAllBallsInPlay, geom = "raster", density1 = list(type = "X"),
+#             density2 = list(quant_score = 1), layer = facet_grid(pitch_type ~ p_throws, labeller = labeller(p_throws = pitch_label)))
+#    
+#    strikeFX(subAllBallsInPlay, geom = "raster", density1 = list(type = "X"),
+#             density2 = list(quant_score = 1), layer = facet_grid(. ~ p_throws, labeller = labeller(p_throws = pitch_label)))
+#    
+#    strikeFX(subAllBallsInPlay, geom = "raster", density1 = list(type = "X"),
+#             density2 = list(type = "X"), layer = facet_grid(. ~ stand, labeller = labeller(p_throws = pitch_label)))
+#    
+#    strikeFX(subAllBallsInPlay, geom = "raster", density1 = list(type = "X"),
+#             density2 = list(quant_score = 1), layer = facet_grid(. ~ stand, labeller = labeller(p_throws = pitch_label)))
+#    
+#    strikeFX(subAllBallsInPlay, geom = "raster", density1 = list(type = "X"),
+#             density2 = list(quant_score = 1), layer = facet_grid(. ~ p_throws, labeller = labeller(p_throws = pitch_label)))
     
     # generate plots
-    create_heatmap_plots(joined,mlbID)
+    create_hv_plots(joined,mlbID)
 }

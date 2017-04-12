6e4c4005ce2295a12ca45b59b7e59cd0271e34c5
Jason Battles <jason.r.battles@gmail.com>
2017-04-12 00:21:01 -0500

    WIP - Nested JSON into DF

--- 00-WIP-Bat.R
+++ 00-WIP-Bat.R
@@ -64,22 +64,45 @@ jsontxt <- content(jsonBtr, "text")
 
 hv_items <- jsontxt %>%
     gather_array %>%                                     # stack the users 
-    spread_values(id = jstring("_id")) %>% 
-    enter_object("pitcher") %>% gather_array %>%       # stack the purchases
-    spread_values(                                       # extract item name and price
-        pitcher.lname = jstring("lastName"),
-        pitcher.fname = jstring("firstName"),
-        pitcher.mlbid = jnumber("mlbid")
-    ) %>%  
+    #spread_values(id = jstring("_id")) %>% 
+    #enter_object("pitcher") %>% gather_array %>%       # stack the purchases
+    #spread_values(                                       # extract item name and price
+    #    pitcher.id = jstring("_id"),
+    #    pitcher.birth = jstring("birthDate"),
+    #    pitcher.lname = jstring("lastName"),
+    #    pitcher.fname = jstring("firstName"),
+    #    pitcher.mlbid = jnumber("mlbid"),
+    #    pitcher.v = jstring("__v")
+    #) %>%  
     spread_values(atbat_des) = jstring("description") %>%
     #enter_object("items") %>% gather_array %>%           # stack the items
     #spread_values(                                       # extract item name and price
     #    item.name = jstring("name"),
     #    item.price = jnumber("price")
     #) %>%
-    select(id, pitcher.lname, pitcher.fname, pitcher.mlbid, atbat_des) # select only what is needed
+    select(atbat_des) # select only what is needed
 
 
 json_data <- fromJSON(json_file, flatten = TRUE)
 as.list(json_data$pitches)
 list <- json_data[1,]
+
+
+
+json01 <- fromJSON("http://67.205.147.49/atbat/batter/58b7a70a997e47000f72fa16")
+json02 <- stream_in(url("http://67.205.147.49/atbat/batter/58b7a70a997e47000f72fa16"))
+
+json01_flat <- flatten(json01)
+library(tibble)
+json01_tbl <- as_data_frame(json01_flat)
+json01_tbl
+
+json01_tbl %>% mutate(pitches = as.character(pitches)) %>% select(pitches)
+
+library(tidyr)
+
+json01_tbl %>% 
+    unnest(pitches) %>%
+    select(pitcher.lastName, batter.lastName, description)
+
+

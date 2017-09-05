25d3b3eb8900efbd2d6ac3c5fc569a0dbe2a8676
Jason R. Battles <jason.r.battles@gmail.com>
2017-09-05 12:46:45 -0500

    updated HittersList.  URL for headshot

---
+++ 21-MachineLearning.R
@@ -0,0 +1,14 @@
+library(caret)
+library(rpart)
+library(rpart.plot)
+library(randomForest)
+library(knitr)
+library(gbm)
+library(ipred)
+library(plyr)
+library(e1071)
+library(kernlab)
+
+set.seed(2121)
+
+in.Train <- createDataPartition(y=)

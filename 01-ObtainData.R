20aa6158d07361039bd7377cd437adb40163f4d7
Jason R. Battles <jason.r.battles@gmail.com>
2018-10-03 15:19:02 -0500

    reversion

--- 01-ObtainData.R
+++ 01-ObtainData.R
@@ -1,3 +1,13 @@
+install.packages("devtools")
+library(devtools)
+install.packages("ggplot2")
+install_github("cpsievert/pitchRx")
+install.packages("pitchRx")
+install.packages("dplyr")
+install.packages("lubridate")
+install.packages("RSQLite")
+
+
 ## load libraries
 library(ggplot2)
 library(graphics)
@@ -11,7 +21,7 @@ library(RSQLite)
 ## Use dplyer to create SQLite database
 #library(dplyr)
 #my_db2016 <- src_sqlite("pitchRx2016.sqlite3", create = TRUE)
-my_dbProd <- src_sqlite("pitchRxProd2018.sqlite3", create = TRUE)
+my_dbProd <- src_sqlite("pitchRxProd082018.sqlite3", create = TRUE)
 
 Today <- Sys.Date()
 ThirtyDaysAgo <- Today - 30
@@ -31,11 +41,13 @@ my_dbProd
 dat1308 <- scrape(start = "2013-08-01", end = "2013-08-01", suffix = "inning/inning_all.xml")
 dat1605 <- scrape(start = "2016-05-01", end = "2016-05-01", suffix = "inning/inning_all.xml")
 dat1611 <- scrape(start = "2016-11-01", end = "2016-11-02", suffix = "inning/inning_all.xml")
-dat1705 <- scrape(start = "2017-05-01", end = "2017-05-05", suffix = "inning/inning_all.xml")
+dat1705 <- scrape(start = "2017-05-01", end = "2017-05-01", suffix = "inning/inning_all.xml")
 dat1707 <- scrape(start = "2017-07-01", end = "2017-07-05", suffix = "inning/inning_all.xml")
 dat1701 <- scrape(start = "2017-01-01", end = "2017-11-30", suffix = "inning/inning_all.xml")
 dat1611 <- scrape(start = "2016-11-01", end = "2016-11-02", suffix = "inning/inning_all.xml")
-dat1804 <- scrape(start = "2018-04-02", end = "2018-04-30", suffix = "inning/inning_all.xml")
+dat1804 <- scrape(start = "2018-04-02", end = "2018-04-03", suffix = "inning/inning_all.xml")
+dat1808 <- scrape(start = "2018-08-01", end = "2018-08-31", suffix = "inning/inning_all.xml")
+
 
 # 2016 season
 #scrape(start = "2016-04-03", end = "2016-11-02", suffix = "inning/inning_all.xml", connect = my_dbProd$con)

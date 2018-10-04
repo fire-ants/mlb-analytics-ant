36f111df24a96363848a9e200b8d5a48e8f35b22
Jason R. Battles <jason.r.battles@gmail.com>
2018-10-04 08:04:14 -0500

    updating master to init dev branch

---
+++ 201810-Testing.R
@@ -0,0 +1,51 @@
+install.packages("devtools")
+library(devtools)
+install.packages("ggplot2")
+install_github("cpsievert/pitchRx")
+
+install.packages("pitchRx")
+install.packages("dplyr")
+install.packages("lubridate")
+install.packages("RSQLite")
+
+
+## load libraries
+library(ggplot2)
+library(graphics)
+library(RColorBrewer)
+library(pitchRx)    ## thank you Carson Sievert!!!
+library(dplyr)      ## thank you Hadley Wickham
+library(stringr)
+library(lubridate)
+library(RSQLite)
+
+## Use dplyer to create SQLite database
+#library(dplyr)
+#my_db2016 <- src_sqlite("pitchRx2016.sqlite3", create = TRUE)
+my_dbProd <- src_sqlite("pitchRxProd082018.sqlite3", create = TRUE)
+
+Today <- Sys.Date()
+ThirtyDaysAgo <- Today - 30
+ThirtyDaysAhead = Today + months(1)
+
+WhatMonth <- month(Today)
+
+#confirm empty
+#my_db2016
+my_dbProd
+
+
+## scrape game data and store in the database
+## 2017 MLB season was from 02APR to 01NOV (inclusive of postseason)
+
+#library(pitchRx)
+dat1308 <- scrape(start = "2013-08-01", end = "2013-08-01", suffix = "inning/inning_all.xml")
+dat160501 <- scrape(start = "2016-05-01", end = "2016-05-01", suffix = "inning/inning_all.xml")
+dat1611 <- scrape(start = "2016-11-01", end = "2016-11-02", suffix = "inning/inning_all.xml")
+dat1705 <- scrape(start = "2017-05-01", end = "2017-05-01", suffix = "inning/inning_all.xml")
+dat170701 <- scrape(start = "2017-07-01", end = "2017-07-01", suffix = "inning/inning_all.xml")
+dat1701 <- scrape(start = "2017-01-01", end = "2017-11-30", suffix = "inning/inning_all.xml")
+dat1611 <- scrape(start = "2016-11-01", end = "2016-11-02", suffix = "inning/inning_all.xml")
+dat180402 <- scrape(start = "2018-04-02", end = "2018-04-02", suffix = "inning/inning_all.xml")
+dat1808 <- scrape(start = "2018-08-01", end = "2018-08-31", suffix = "inning/inning_all.xml")
+
---
+++ PkgCheckpoint.R
@@ -0,0 +1,71 @@
+## Install pitchRx directly from GitHub
+install.packages("devtools")
+library(devtools)
+install_github("cpsievert/pitchRx", force = T)
+
+## This R command lists all the packages installed by the user 
+## (ignoring packages that come with R such as base and foreign) and the 
+## package versions.
+ip <- as.data.frame(installed.packages()[,c(1,3:4)])
+rownames(ip) <- NULL
+ip <- ip[is.na(ip$Priority),1:2,drop=FALSE]
+print(ip, row.names=FALSE)
+
+## Create a checkpoint list of all installed packages
+## https://cran.r-project.org/web/packages/checkpoint/vignettes/checkpoint.html
+install.packages("checkpoint")
+library(checkpoint)
+checkpoint("2018-10-02")
+
+## check which packages are installed in checkpoint library:
+installed.packages(.libPaths()[1])[, "Package"]
+
+## cleanup
+
+unlink(example_project, recursive = TRUE)
+unlink(file.path(tempdir(), "checkpoint_example_code.R"))
+unlink(file.path(tempdir(), ".checkpoint"), recursive = TRUE)
+options(repos = oldRepos)
+unCheckpoint(oldLibPaths)
+.libPaths()
+
+## [1] "C:/tmp/Rtmp69rJVn/Rinst1e3c1470709e"          
+## [2] "C:/Users/richcala/Documents/R/win-library/3.5"
+## [3] "C:/Program Files/R/R-3.5.1/library"
+
+
+
+## Create a folder to contain the checkpoint
+## This is optional - the default is to use ~/.checkpoint
+
+dir.create(file.path(tempdir(), ".checkpoint"), recursive = TRUE, showWarnings = FALSE)
+
+## Create a checkpoint by specifying a snapshot date
+
+library(checkpoint)
+checkpoint("2018-10-02", project = mlb-analytics-ant, 
+           checkpointLocation = tempdir())
+
+#cleanup
+unlink(file.path(tempdir(), ".checkpoint"), recursive = TRUE)
+
+
+# create a list of all installed packages
+ip <- as.data.frame(installed.packages())
+head(ip)
+
+# if you use MRO, make sure that no packages in this library will be removed
+ip <- subset(ip, !grepl("MRO", ip$LibPath))
+
+# we don't want to remove base or recommended packages either\
+ip <- ip[!(ip[,"Priority"] %in% c("base", "recommended")),]
+
+# determine the library where the packages are installed
+path.lib <- unique(ip$LibPath)
+
+# create a vector with all the names of the packages you want to remove
+pkgs.to.remove <- ip[,1]
+head(pkgs.to.remove)
+
+# remove the packages
+sapply(pkgs.to.remove, remove.packages, lib = path.lib)
